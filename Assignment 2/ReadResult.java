e1619ae0534922ac8a5548e0ce2c0ecd97ff50b3
lamanasgarli <118808533+lamanasgarli@users.noreply.github.com>
2026-03-04 21:22:06 +0400

    ReadResult.java

--- Assignment 2/ReadResult.java
+++ Assignment 2/ReadResult.java
@@ -1,1 +1,30 @@
+package ringbuffer;
 
+import java.util.Optional;
+
+public final class ReadResult<T> {
+    private final T item;              // can be null if nothing available
+    private final long missedCount;    // number of items skipped due to overwrite
+
+    public ReadResult(T item, long missedCount) {
+        this.item = item;
+        this.missedCount = missedCount;
+    }
+
+    public Optional<T> getItem() {
+        return Optional.ofNullable(item);
+    }
+
+    public long getMissedCount() {
+        return missedCount;
+    }
+
+    public boolean hasItem() {
+        return item != null;
+    }
+
+    @Override
+    public String toString() {
+        return "ReadResult{item=" + item + ", missedCount=" + missedCount + "}";
+    }
+}
